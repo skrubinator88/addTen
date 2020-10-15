d906d2773e99218b1f285ec250ff9e916f19138b
Oseriemen Ivbaze <oivbaze1@student.gsu.edu>
2020-10-15 02:02:44 -0400

    updated for empty input edge case

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 app.get('/:digit', function (req, res) {
-  res.send(req.params.digit + 10);
+  if(req.params.digit) {
+    res.send(req.params.digit + 10);
+  } else {
+    res.send("No input recieved");
+  }
 });
 app.listen(3000, function () {
   console.log('Listening on port 3000!');
